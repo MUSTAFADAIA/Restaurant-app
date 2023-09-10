2cd004e704ace519a9c9074259d798df440cf376
Mustafa Abu Daia <88327399+MUSTAFADAIA@users.noreply.github.com>
2023-09-10 19:04:06 +0300

    Update restaurantValidator.js

--- utils/validators/restaurantValidator.js
+++ utils/validators/restaurantValidator.js
@@ -18,7 +18,7 @@ exports.creatRestaurantValidator = [
   check("category")
     .notEmpty()
     .withMessage("Restaurant must be belong to a category")
-    .isLength()
+    .isMongoId()
     .withMessage("Invalid ID formate"),
 
   check("city")
