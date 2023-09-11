4842d2f3217dd7f5c955d776e172edc9b510825d
Mustafa Abu Daia <88327399+MUSTAFADAIA@users.noreply.github.com>
2023-09-11 20:50:04 +0300

    Update reservationsModels.js

--- models/reservationsModels.js
+++ models/reservationsModels.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const reservationSchema = mongoose.Schema(
   {
     user: {
-      type: mongoose.Schema.Types.ObjectId,
+      type:[Array],
+        //mongoose.Schema.Types.ObjectId,
       ref: "user",
     },
     restaurant: {
